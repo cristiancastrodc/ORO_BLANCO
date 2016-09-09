026ff28288d81afe772dfd3d2f80abfcbdc0fec9
Cristian Castro <cristiancastrodc@gmail.com>
2016-09-08 23:20:41 -0500

    Instalando Laravel Collective

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -10,16 +10,60 @@
 <body>
   <div class="container">
     <div class="row">
-      <h1>Crear Usuario</h1>
-      <form action="">
-        
-      </form>
+      <div class="col-sm-8">
+        <h1>Crear Usuario</h1>
+        <form class="form-horizontal" method="POST" action="{{ url('/admin/usuarios/store') }}">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <div class="form-group">
+            <label for="tbUser" class="col-sm-2 control-label">Usuario:</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="tbUser" name="tbUser" placeholder="Usuario">
+              <span class="help-block">Ingrese el usuario para el inicio de sesión.</span>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="tbPassword" class="col-sm-2 control-label">Contraseña:</label>
+            <div class="col-sm-10">
+              <input type="password" class="form-control" id="tbPassword" name="tbPassword" placeholder="Contraseña">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="tbDNI" class="col-sm-2 control-label">DNI:</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="tbDNI" name="tbDNI" placeholder="DNI">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="tbFirstName" class="col-sm-2 control-label">Nombres:</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="tbFirstName" name="tbFirstName" placeholder="Nombres">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="tbLastName" class="col-sm-2 control-label">Apellidos:</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="tbLastName" name="tbLastName" placeholder="Apellidos">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="selRole" class="col-sm-2 control-label">Rol:</label>
+            <div class="col-sm-10">
+              <select class="form-control" id="selRole" name="selRole">
+                <option>Administrador</option>
+                <option>Ventas</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <button type="submit" class="btn btn-primary">Guardar</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="{{ asset('https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js') }}"></script>
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa') }}" crossorigin="anonymous"></script>
+  <script src="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js') }}" ></script>
 </body>
 </html>
