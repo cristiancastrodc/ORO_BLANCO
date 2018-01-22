b19cbfd5796acd9887f7fb8aa152453dde082850
Cristian Castro <cristiancastrodc@gmail.com>
2018-01-22 11:56:07 -0500

    Files updated due to .gitignore changed.

--- db/dbupdate-2016-10-11.sql
+++
@@ -1,6 +0,0 @@
--- Correr las migraciones hasta antes de 2016_10_11_225228_set_default_value_to_category_id.php
--- Crear la categoría 'sin categoría'
-INSERT INTO categories(nombre) VALUES ('sin categoria');
--- Actualizar la categoría para todos los productos
-UPDATE products SET id_categoria = 1;
-/** Luego de estas consultas, realizar la migración set_default_value_to_category_id **/
